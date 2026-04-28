c95b536426197849de734c8bad280ff86c1bd138
Atiq <atiqd400@gmail.com>
2026-04-28 14:48:45 +0500

    sample file

---
+++ multiply.py
@@ -0,0 +1,8 @@
+print('enter two numbers and i will display the product of them: ')
+
+a = int(input('enter first number'))
+b = int(input('enter second number'))
+
+product = a * b
+
+print('product: ', product)
