469baa382dd44a67a1e456f85f43f14ff939e1e9
Atiq <atiqd400@gmail.com>
2026-04-28 14:23:36 +0500

    test file

---
+++ sum.py
@@ -0,0 +1,8 @@
+print('enter two numbers and i will display the sum of them: ')
+
+a = int(input('enter first number'))
+b = int(input('enter second number'))
+
+sum = a + b
+
+print('the sum of these numbers will be: ', sum)
